9e7cac6d446e428cef3532534df17001619414bd
hithesh svsk <8svskhd@gmail.com>
2020-12-21 23:08:17 +0530

    6th commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Hithesh SVSK : Portfolio`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `My Digital presence`,
     author: `@gatsbyjs`,
   },
   plugins: [
